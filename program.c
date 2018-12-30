fa807cf198a9a39b30ce5f258516aa0e7fc0fd23
Valaraucoo <jestem.kamil.wozniak@gmail.com>
2018-12-30 17:17:51 +0100

    dodanie usuwania

--- program.c
+++ program.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
-
+#define MAX 255
 
 struct Person {
     char *full_name;
@@ -28,13 +28,6 @@ struct Person* createPerson(char* name,char* pesel){
     
     return newPerson;
 }
-
-// do robienia pierwszego wezla
-/*struct PersonNode* createPersonNode(){
-    struct PersonNode *newPersonNode =(struct PersonNode*)malloc(sizeof(struct PersonNode));
-    newPersonNode->next = NULL; // wskaznik na potomka - pusty
-    return newPersonNode;
-}*/
 
 // przyjmuje start listy , zwraca ostatni element
 struct PersonNode* getLastPersonNode(struct PersonNode* list){
@@ -68,18 +61,34 @@ void addNewPersonNode(struct PersonNode* list,struct Person* person){
 }
 
 
-void fillPersonList(struct PersonNode* pNode){
-    char full_name_buffer[255];
-    char pesel_buffer[255];
-    char buffer[2];
 
+void fillPersonList(struct PersonNode* pNode){
+
+    char buffer[2];
+    int len;
+    char* full_name;
+    char* pesel;
+    
     printf("\nCzy chcesz dodac osobe do listy? [y/n]: ");
     while(strcmp(gets(buffer),"n")!=0){
+        
         printf("Podaj imie i nazwisko>> ");
+        char *full_name_buffer = (char *)malloc((sizeof(char)) * MAX);
         gets(full_name_buffer);
+        len = strlen(full_name_buffer);
+        full_name = (char*)malloc(sizeof(char)*len + 1);
+        strcpy(full_name,full_name_buffer);
+        free(full_name_buffer);
+        
         printf("Podaj pesel>> ");
+        char *pesel_buffer = (char *)malloc((sizeof(char))* MAX);
         gets(pesel_buffer);
-        addNewPersonNode(pNode,createPerson(full_name_buffer,pesel_buffer));
+        len = strlen(pesel_buffer);
+        pesel = (char*)malloc(sizeof(char) * len +1);
+        strcpy(pesel,pesel_buffer);
+        free(pesel_buffer);
+        
+        addNewPersonNode(pNode,createPerson(full_name,pesel));
         
         printf("\nCzy chcesz kontunuowac dodawanie?[y/n]: ");
     }
@@ -89,12 +98,40 @@ void printPeople(struct PersonNode* pNode){
     printf("\n==========================\n");
     //wez liste podan
     struct PersonNode* element = pNode->next;
+    int i = 1;
     while(element!=NULL){
-        printf("\n\nImie i nazwisko: %s\nPesel: %s",element->person->full_name,element->person->pesel);
+        printf("\n\n%d.Imie i nazwisko: %s\nPesel: %s",i,element->person->full_name,element->person->pesel);
         element = element->next;
+        i++;
     }
 }
 
+int dlugoscListy(struct PersonNode* pNode){
+    int d=0;
+    struct PersonNode* element = pNode->next;
+
+    while(element!=NULL){
+        element = element->next;
+        d++;
+    }
+    return d;
+}
+
+void usunElement(struct PersonNode* pNode,int n){
+    struct PersonNode* wsk = pNode;
+    int i =0;
+    while(wsk->next != NULL){
+        if(i==n-1){
+            struct PersonNode* usuwany = wsk->next;
+            wsk->next = usuwany->next;
+            free(usuwany);
+        }else{
+            wsk = wsk->next;
+        }
+        
+        i++;
+    }
+}
 
 
 
@@ -109,9 +146,17 @@ int main(void) {
     
     printPeople(pNode);
     
+    printf("\n\nDLUGOSC : %d",dlugoscListy(pNode));
     
+    int d;
+    printf("\nKtory element usunac? ");
+    scanf("%d",&d);
     
+    usunElement(pNode,d);
     
+    printf("\n\nDLUGOSC : %d",dlugoscListy(pNode));
+    
+    printPeople(pNode);
     
     
     return 0;
